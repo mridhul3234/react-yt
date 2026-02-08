63ea495a817e420a2cf610f5eac5491ca50e70bb
Mridhul Vashisht <mridhul3234@gmail.com>
2026-02-08 12:28:58 +0530

    commiting before making props project

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <div className="card">
+        <img src="https://images.unsplash.com/photo-1770306925042-05987831cb6e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Sarthak Sharma</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default card
