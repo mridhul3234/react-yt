32ac643a6cf9c01fdb9642094897dc39e67c81e3
Mridhul Vashisht <mridhul3234@gmail.com>
2026-03-07 19:33:26 +0530

    Adhi Adhuri Routing

--- 18-React-RoutingDOMbasics/src/App.jsx
+++ 18-React-RoutingDOMbasics/src/App.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Contact from './pages/Contact'
 import About from './pages/About'
 import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2'
+import Courses from './pages/Courses'
+import Kids from './pages/'
 
 const App = () => {
   
   return (
     <div>
       <Navbar/> 
+      <Navbar2/>
       <Routes>
         <Route    path='/' element= {<Home/>} />
-        <Route    path='/contact' element= {<Contact/>} />
         <Route    path='/about' element= {<About/>} />
+        <Route    path='/courses' element= {<Courses/>} />
+
+        <Route path='/product ' element={<Product/>}>
+          <Route path='men' element={<Product />}/>
+          <Route path='women' element={<Women />}/>
+          <Route path='kids' element={<Kids />}/>
+        </Route>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </div>
   )
--- 18-React-RoutingDOMbasics/src/components/Navbar.jsx
+++ 18-React-RoutingDOMbasics/src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='nav'>
-      <h3>Sheriyans</h3>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
-      </div>
+    <div className='flex py-4 items-center px-8 bg-cyan-900 justify-between'>
+        <h2 className='text-2xl font-bold'>Sheryians</h2>
+        <div className='flex gap-10'>
+          <Link className='text-lg font-medium' to='/'>Home</Link>
+          <Link className='text-lg font-medium' to='/about'>About</Link>
+          <Link className='text-lg font-medium' to='/courses'>Courses</Link>
+          <Link className='text-lg font-medium' to='/product'>Product</Link>
+        </div>
     </div>
   )
 }
